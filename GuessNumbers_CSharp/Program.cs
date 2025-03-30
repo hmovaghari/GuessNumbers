e78b4466f0c840ffe71861237b3c1e9b930fe5b7
H Movaghari <hmovaghari@gmail.com>
2025-03-30 16:33:49 +0330

    Generate 4 Digit Random Number

--- GuessNumbers_CSharp/Program.cs
+++ GuessNumbers_CSharp/Program.cs
@@ -1,6 +1,33 @@
-﻿do
+﻿
+do
 {
+    /* Generate4DigitRandomNumber */
+    List<string> randomNumbers = Generate4DigitRandomNumber();
+    /* Generate4DigitRandomNumber */
+
     /* Play again? */
     Console.WriteLine("Play again? y or n");
 } while (Console.ReadLine()?.ToLower() == "y");
 /* Play again? */
+
+List<string> Generate4DigitRandomNumber()
+{
+    var result = new List<string>();
+    
+    var numbers = new List<string>();
+    for (int i = 0; i < 10; i++)
+    {
+        numbers.Add(i.ToString());
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        Random random = new Random();
+        var from = i == 0 ? 1 : 0;
+        var index = random.Next(from, numbers.Count);
+        result.Add(numbers[index]);
+        numbers.RemoveAt(index);
+    }
+
+    return result;
+}
--- GuessNumbers_Rust/src/main.rs
+++ GuessNumbers_Rust/src/main.rs
@@ -1,7 +1,14 @@
+use std::vec;
+use rand::Rng;
+
 fn main()
 {
     loop
     {
+        /* Generate4DigitRandomNumber */
+        let randomNumbers: Vec<String> = Generate4DigitRandomNumber();
+        /* Generate4DigitRandomNumber */
+
         /* Play again? */
         println!("Play again? y or n");
         let mut input: String = String::new();
@@ -12,4 +19,29 @@ fn main()
         }
     }
     /* Play again? */
+}
+
+fn Generate4DigitRandomNumber() -> Vec<String>
+{
+    let mut result: Vec<String> = Vec::new();
+
+    let mut numbers: Vec<String> = Vec::new();
+    let mut counter = 0;
+    while counter < 10
+    {
+        numbers.push(format!("{counter}"));
+        counter += 1;
+    }
+
+    counter = 0;
+    while counter < 4
+    {
+        let from = if counter == 0 {1} else {0};
+        let index = rand::thread_rng().gen_range(from..numbers.len());
+        result.push(numbers[index].clone());
+        numbers.remove(index);
+        counter += 1;
+    }
+
+    result
 }
